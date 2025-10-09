433319c5cd4ebe6e603e777ec1e5f516566005cf
Ozzy-ZY <zyadhimself1@gmail.com>
2025-10-09 19:00:40 +0300

    Add JPA entities for Ticket, TicketType, Attendee, and Event

---
+++ tickets-hall/tickets/src/main/java/com/ticketshall/tickets/models/Event.java
@@ -0,0 +1,26 @@
+package com.ticketshall.tickets.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public final class Event {
+    @Id
+    UUID id;
+    String name;
+    LocalDateTime startDate;
+    LocalDateTime endDate;
+    String venueName;
+    String organizerId;
+}
