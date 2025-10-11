58f9038b7ed088cfba87c3d8a0d2c7995cc18f9d
Mustafa-Mahmoud-5 <mustafamahmoud4004@gmail.com>
2025-10-11 04:42:00 +0300

    chore(events): setup the data-jpa configs

--- tickets-hall/events/src/main/java/com/ticketshall/events/EventsApplication.java
+++ tickets-hall/events/src/main/java/com/ticketshall/events/EventsApplication.java
@@ -1,11 +1,15 @@
 package com.ticketshall.events;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class EventsApplication {
     public static void main(String[] args) {
+        log.info("Events Service Started...");
         SpringApplication.run(EventsApplication.class, args);
     }
 }
