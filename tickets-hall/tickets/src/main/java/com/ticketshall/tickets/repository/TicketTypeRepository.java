6c7e48731390a04b1b254017e8622f07c7252596
Ozzy-ZY <zyadhimself1@gmail.com>
2025-10-13 00:09:25 +0300

    Add method to fetch TicketTypes by event ID in TicketTypeRepository

--- tickets-hall/tickets/src/main/java/com/ticketshall/tickets/repository/TicketTypeRepository.java
+++ tickets-hall/tickets/src/main/java/com/ticketshall/tickets/repository/TicketTypeRepository.java
@@ -3,8 +3,10 @@ package com.ticketshall.tickets.repository;
 import com.ticketshall.tickets.models.TicketType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TicketTypeRepository extends JpaRepository<TicketType, UUID> {
 
+    List<TicketType> getTicketTypesByEventId(UUID eventId);
 }
