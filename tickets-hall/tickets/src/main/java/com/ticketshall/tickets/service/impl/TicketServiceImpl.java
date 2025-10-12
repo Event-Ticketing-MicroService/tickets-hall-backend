619499110163f5f381efcf20a8cec3f6d43a6508
Ozzy-ZY <zyadhimself1@gmail.com>
2025-10-13 00:09:16 +0300

    Implement `TicketService` with methods for creating and listing `TicketType` entities, including Redis cache integration.

---
+++ tickets-hall/tickets/src/main/java/com/ticketshall/tickets/service/impl/TicketServiceImpl.java
@@ -0,0 +1,54 @@
+package com.ticketshall.tickets.service.impl;
+
+import com.ticketshall.tickets.dto.CreateTicketTypeRequest;
+import com.ticketshall.tickets.mapper.TicketTypeMapper;
+import com.ticketshall.tickets.models.Ticket;
+import com.ticketshall.tickets.models.TicketType;
+import com.ticketshall.tickets.repository.EventRepository;
+import com.ticketshall.tickets.repository.TicketTypeRepository;
+import com.ticketshall.tickets.service.TicketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class TicketServiceImpl implements TicketService {
+    private final TicketTypeRepository ticketTypeRepository;
+    private final EventRepository eventRepository;
+    private final TicketTypeMapper ticketTypeMapper;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private static final String TICKET_TYPE_INFIX = ":ticketType:";
+    private static final String EVENT_PREFIX = "event:";
+    @Override
+    public TicketType createTicketType(CreateTicketTypeRequest request){
+        if(!eventRepository.existsById(request.eventId())){
+            throw new IllegalArgumentException("Event not found: " + request.eventId()); // TODO: Create Exceptions
+        }
+        var ticketType = ticketTypeMapper.toTicketType(request);
+        ticketTypeRepository.save(ticketType);
+        return ticketType;
+    }
+    // TODO: Keep cache valid throughout various ops on TicketType
+    @Override
+    public List<TicketType> listTicketTypesForEvent(UUID eventId) {
+        var keys = redisTemplate.keys(EVENT_PREFIX + eventId + TICKET_TYPE_INFIX + "*");
+        List<TicketType> result = new java.util.ArrayList<>();
+        if (keys.isEmpty()) { // cache miss
+            result = ticketTypeRepository.getTicketTypesByEventId(eventId);
+            result.forEach(ticketType -> redisTemplate.opsForValue()
+                    .set(EVENT_PREFIX + eventId + TICKET_TYPE_INFIX + ticketType.getId(), ticketType));
+        }
+        for (String key: keys){
+            var ticketType = (TicketType) redisTemplate.opsForValue().get(key);
+            if (ticketType != null) {
+                result.add(ticketType);
+            }
+        }
+        return result;
+    }
+
+}
