624c19ffc4098910e9d9b591af7e42aaec29bcb3
OmarEhab14 <www.omarehab3710@gmail.com>
2025-10-13 12:11:19 +0300

    Add: General constants class for global constants.

---
+++ tickets-hall/tickets/src/main/java/com/ticketshall/tickets/models/nonStoredModels/constants/GeneralConstants.java
@@ -0,0 +1,7 @@
+package com.ticketshall.tickets.models.nonStoredModels.constants;
+
+public class GeneralConstants {
+    public static final String REDIS_RESERVATION_PREFIX = "reservation:";
+    public static final String REDIS_TICKET_TYPE_INFIX = ":ticketType:";
+    public static final String REDIS_EVENT_PREFIX = "event:";
+}
