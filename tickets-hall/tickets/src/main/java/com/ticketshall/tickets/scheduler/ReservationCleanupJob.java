1ed6fad9051630429bf1fb656373ba5157792ac0
OmarEhab14 <www.omarehab3710@gmail.com>
2025-10-13 12:12:16 +0300

    Completed: Reservation cleanup cron job.

--- tickets-hall/tickets/src/main/java/com/ticketshall/tickets/scheduler/ReservationCleanupJob.java
+++ tickets-hall/tickets/src/main/java/com/ticketshall/tickets/scheduler/ReservationCleanupJob.java
@@ -1,10 +1,15 @@
 package com.ticketshall.tickets.scheduler;
 
+import com.ticketshall.tickets.models.nonStoredModels.Reservation;
 import com.ticketshall.tickets.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -13,8 +18,17 @@ public class ReservationCleanupJob {
     private final RedisTemplate<String, Object> redisTemplate;
     private static final String RESERVATION_PREFIX = "reservation:";
 
-    @Scheduled(fixedRate = 30000)
-    public void cleanup() {
+    @Scheduled(fixedRate = 60000)
+    public void cleanupExpiredReservations() {
+        Set<String> keys = redisTemplate.keys("reservation:*");
+        for (String key : keys) {
+            Reservation reservation = (Reservation) redisTemplate.opsForValue().get(key);
+            if (reservation == null) continue;
 
+            if (reservation.getExpiresAtUtc().isBefore(LocalDateTime.now())) {
+                // ToDo: Cancel Stripe session
+                reservationService.expireReservation(reservation.getId()); // Restore stock
+            }
+        }
     }
 }
