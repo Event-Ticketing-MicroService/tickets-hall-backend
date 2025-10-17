446c4a5e60ab4f83762dd2a00b46afaac232adf4
Mustafa-Mahmoud-5 <mustafamahmoud4004@gmail.com>
2025-10-17 06:02:18 +0300

    feat(events-ticket-types): implemented ticket-type dto, validations and controller

--- tickets-hall/events/src/main/java/com/ticketshall/events/exceptions/GlobalExceptionHandler.java
+++ tickets-hall/events/src/main/java/com/ticketshall/events/exceptions/GlobalExceptionHandler.java
@@ -2,12 +2,14 @@ package com.ticketshall.events.exceptions;
 
 import com.ticketshall.events.dtos.responses.ApiErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
@@ -37,15 +39,36 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        System.out.println("hithit");
         log.info("Validations Error: {}", e.getMessage(), e);
         String firstError = "invalid field '" + e.getFieldErrors().get(0).getField() + "': " + e.getFieldErrors().get(0).getDefaultMessage();
         ApiErrorResponse errorResponse = new ApiErrorResponse("BAD_REQUEST", firstError, HttpStatus.BAD_REQUEST.value());
         return ResponseEntity.status(errorResponse.getStatusCode()).body(errorResponse);
     }
 
+    // this is triggered by @Valid when we put validation annotations on the Controller/Handler params
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<?> handleHandlerMethodValidationException(HandlerMethodValidationException e) {
+        log.info("Validation Error: {}", e.getMessage(), e);
+
+        // Extract details of the validation failure
+        String errors = e.getParameterValidationResults()
+                .stream()
+                .flatMap(vr -> vr.getResolvableErrors().stream())
+                .map(err -> err.getDefaultMessage())
+                .reduce((a, b) -> a + ", " + b)
+                .orElse("Validation failed");
+
+        ApiErrorResponse response = new ApiErrorResponse(
+                "BAD_REQUEST", errors, HttpStatus.BAD_REQUEST.value());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiErrorResponse> handleUnexpectedException(Exception e) {
         log.error("Internal Server Error: {}", e.getMessage(), e);
+        e.printStackTrace();
         ApiErrorResponse errorResponse = new ApiErrorResponse("INTERNAL_SERVER_ERROR", "Something Went Wrong", HttpStatus.INTERNAL_SERVER_ERROR.value());
         return ResponseEntity.status(errorResponse.getStatusCode()).body(errorResponse);
     }
--- tickets-hall/events/src/main/java/com/ticketshall/events/mappers/CategoryMapper.java
+++ tickets-hall/events/src/main/java/com/ticketshall/events/mappers/CategoryMapper.java
@@ -7,5 +7,4 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
     Category toCategory(CategoryParams categoryParams);
-//    CategoryResponse toCategoryResponse(Category category);
 }
