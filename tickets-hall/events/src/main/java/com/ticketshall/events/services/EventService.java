5ae04e4c77d15029297c5d289d8307fa1c145388
Mustafa-Mahmoud-5 <mustafamahmoud4004@gmail.com>
2025-10-30 11:34:14 +0300

    feat: implemented a get all events endpoint with filters, pagination and sorting

--- tickets-hall/events/src/main/java/com/ticketshall/events/services/EventService.java
+++ tickets-hall/events/src/main/java/com/ticketshall/events/services/EventService.java
@@ -1,11 +1,15 @@
 package com.ticketshall.events.services;
 
 import com.ticketshall.events.dtos.params.CreateEventParams;
+import com.ticketshall.events.dtos.filterparams.EventFilterParams;
 import com.ticketshall.events.models.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
 
 public interface EventService {
     Event createEvent(CreateEventParams createEventParams);
     Event getEvent(UUID id);
+    Page<Event> getAllEvents(EventFilterParams eventFilterParams, Pageable pageable);
 }
