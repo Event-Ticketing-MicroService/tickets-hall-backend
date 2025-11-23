f6bde507144435de44e24a312717285071421e72
OmarEhab14 <www.omarehab3710@gmail.com>
2025-11-23 18:49:31 +0200

    Modify: make outbox scheduled job run every 10 seconds

--- tickets-hall/events/src/main/java/com/ticketshall/events/repositories/OutboxRepository.java
+++ tickets-hall/events/src/main/java/com/ticketshall/events/repositories/OutboxRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface OutboxRepository extends JpaRepository<OutboxMessage, Long> {
-    List<OutboxMessage> findByProcessedFalseAndType(String type);
+    List<OutboxMessage> findTop10ByProcessedFalseAndType(String type);
 }
