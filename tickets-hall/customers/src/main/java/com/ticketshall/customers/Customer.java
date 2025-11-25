67b23d6793e0af1e7f01879680b8dce964dc45e8
Ozzy-ZY <zyadhimself1@gmail.com>
2025-11-25 14:32:46 +0200

    Refactor: simplify Customer entity using Lombok annotations

--- tickets-hall/customers/src/main/java/com/ticketshall/customers/Customer.java
+++ tickets-hall/customers/src/main/java/com/ticketshall/customers/Customer.java
@@ -1,101 +1,26 @@
 package com.ticketshall.customers;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Entity
-@Table
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String email;
     private String password;
     private String name;
     private LocalDate dob;
     private String phoneNumber;
     private String role;
-
-    public Customer(){}
-
-    public Customer(Long id, String email, String password, String name, LocalDate dob, String phoneNumber, String role) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.name = name;
-        this.dob = dob;
-        this.phoneNumber = phoneNumber;
-        this.role = role;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", name='" + name + '\'' +
-                ", dob=" + dob +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", role='" + role + '\'' +
-                '}';
-    }
 }
