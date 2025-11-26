390ddc64645ffab510d9ce528579ae91a19be65d
OmarEhab14 <www.omarehab3710@gmail.com>
2025-11-26 14:02:40 +0200

    feat(venues): add constants and utility helpers - Add GeneralConstants for application-wide constants - Add JsonUtil for JSON serialization/deserialization

---
+++ tickets-hall/venues/src/main/java/com/ticketshall/venues/constants/GeneralConstants.java
@@ -0,0 +1,6 @@
+package com.ticketshall.venues.constants;
+
+public class GeneralConstants {
+    public static String WORKER_CREATED_OUTBOX_TYPE = "WORKER_CREATED";
+    public static String WORKER_UPDATED_OUTBOX_TYPE = "WORKER_UPDATED";
+}
